ca3094aa23ae6ddfd3d4b6590f33967a560b7be2
Tanvir Hossen Raju <103171957+exploreTanvir@users.noreply.github.com>
2024-05-12 22:57:10 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
